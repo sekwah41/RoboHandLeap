baae47c4acae3e57eb12b8a4888a07c75f78911b
Alastair Hawkes <alastair.hawkes@gmail.com>
2017-02-19 16:19:42 +0000

    replaced code

--- Java/com/sekwah/robohand/hand/FingerAnaly.java
+++ Java/com/sekwah/robohand/hand/FingerAnaly.java
@@ -35,12 +35,20 @@ public class FingerAnaly {
     }
 
     public static float anglePoints(Finger finger, Finger.Joint joint1,  Finger.Joint joint2,  Finger.Joint joint3){
-        return finger.jointPosition(joint1).minus(finger.jointPosition(joint2))
-                .angleTo(finger.jointPosition(joint3).minus(finger.jointPosition(joint2)));
+        Vector vec = finger.jointPosition(joint1).minus(finger.jointPosition(joint2));
+        Vector vec2 = finger.jointPosition(joint3).minus(finger.jointPosition(joint2));
+
+        float crossProd = vec.getX() * vec2.getX() + vec.getZ() * vec2.getZ() + vec.getY() * vec2.getY();
+
+        float length = getLength(vec);
+
+        float length2 = getLength(vec2);
+
+        return (float) Math.acos(crossProd / (length * length2));
     }
 
-    public static float angle(Vector vec1, Vector vec2){
-        return vec1.angleTo(vec2);
+    private static float getLength(Vector vec) {
+        return (float) Math.sqrt(vec.getX() * vec.getX() + vec.getY() * vec.getY() + vec.getZ() * vec.getZ());
     }
 
 }
