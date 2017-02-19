1843c39e0450dfe63ca062c3801350f94e1b2155
Alastair Hawkes <alastair.hawkes@gmail.com>
2017-02-19 07:56:56 +0000

    Base files

---
+++ Arduino/Motor_Select_Test/Motor_Select_Test.ino
@@ -0,0 +1,69 @@
+#include <Servo.h>
+
+// 0-4 fingers 5 for thumb pinkie first to index. (use right hand left to right when facing towards you)
+Servo finger[5];
+// Max angle is 179 (dont try 180...)
+int angle = 1;
+
+void setup() {
+  finger[0].attach(3);
+  finger[1].attach(5);
+  finger[2].attach(6);
+  finger[3].attach(9);
+  finger[4].attach(10);
+  // Initialise arm positions
+  for (int i = 0; i <= 4; i = i + 1) {
+    finger[i].write(0);
+  }
+
+  delay(400);
+
+  for (int i = 0; i <= 4; i = i + 1) {
+    finger[i].write(179);
+    delay(400);
+    finger[i].write(0);
+  }
+
+  Serial.begin(9600);
+  Serial.println("Init");
+  //delay(10000);
+
+}
+
+void loop() {
+  // Loop to get the data from the java program
+  //Serial.println("Test");
+  /**for (int i = 0; i <= 4; i = i + 1) {
+    Serial.println(i);
+    finger[i].write(159);
+    delay(2000);
+    finger[i].write(10);
+    delay(2000);
+    }*/
+
+  if (Serial.available() > 0) {
+    int text = Serial.read() - 48 - 1;
+
+    if ( text >= 0 && text <= 4) {
+      finger[text].write(179);
+      delay(400);
+      finger[text].write(0);
+    }
+
+    Serial.println(text);
+
+  }
+
+  //Serial.println(i);
+  /**for (int i = 0; i <= 4; i = i + 1) {
+    delay(50);
+    finger[i].write(159);
+    }
+    delay(1000);
+    for (int i = 0; i <= 4; i = i + 1) {
+    delay(50);
+    finger[i].write(10);
+    }
+    delay(2000);*/
+
+}
---
+++ Java/com/sekwah/robohand/Main.java
@@ -0,0 +1,12 @@
+package com.sekwah.robohand;
+
+/**
+ * @author sekwah41
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
